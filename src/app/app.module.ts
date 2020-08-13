3e59df88c41e052fe9f49b3209d0b346fd4cb6e1
John Long <johnlong@amcsgroup.com>
2020-08-13 09:08:22 +0100

    created base components for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PostsComponent } from './posts/posts.component';
+import { FeedComponent } from './feed/feed.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostsComponent,
+    FeedComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
