eebc5d4745d7da4ca0232474d215d1ee1e9ae37d
jbo168 <john.chris.long@gmail.com>
2020-08-15 22:33:10 +0100

    added like button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FeedItemComponent } from './feed/feed-list/feed-item/feed-item.component';
 import { FeedListComponent } from './feed/feed-list/feed-list.component';
 import { FeedService } from './feed/feed.service';
+import { LikeButtonComponent } from './feed/feed-list/feed-item/like-button/like-button.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FeedService } from './feed/feed.service';
     FeedComponent,
     ProfileComponent,
     FeedItemComponent,
-    FeedListComponent
+    FeedListComponent,
+    LikeButtonComponent
   ],
   imports: [
     BrowserModule,
