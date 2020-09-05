8e3253218794168442b3230f931905ecd60f3188
jbo168 <john.chris.long@gmail.com>
2020-09-05 16:32:21 +0100

    adding profile form

---
+++ src/app/profile/profile-form/profile-form.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-profile-form',
+    templateUrl: './profile-form.component.html',
+    styleUrls: ['./profile-form.component.css']
+  })
+export class ProfileFormComponent implements OnInit {
+
+    constructor() {}
+
+    ngOnInit() {}
+}
